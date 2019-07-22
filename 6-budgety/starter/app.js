955c69e52c662517ac4b56665160af52c4995d66
Alan Chu <alan_chu@yahoo.com>
2019-07-22 16:34:32 +1000

    Improved event handling in the main controller

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -11,9 +11,24 @@ var UIcontroller = (function(){
 
 // Global App Controller
 var controller = (function(){
-	// Detection of input and Add button
-	var userInput = document.querySelector(".add__description");
+
+	var ctrlAddItem = function() {
+		// 1. Get field input data
+		// 2. Add item to budget controller
+		// 3. Add item to the UI
+		// 4. Calculate the budget
+		// 5. Display the budget on the UI		
+		alert('ctrl get item called');			
+	}
+
+	// Detection of global input and Add button
 	var addButton = document.querySelector(".add__btn");
-	userInput.addEventListener('click',function(){alert('userInput was clicked');});
-	addButton.addEventListener('click',function(){alert('addButton was clicked');});
+	document.addEventListener('keypress',function(event){
+		if (event.keyCode === 13 || event.which === 13){
+			ctrlAddItem();
+		}
+	});
+
+	addButton.addEventListener('click',ctrlAddItem);
+
 })(budgetController, UIcontroller);
